2f63c1e6bd1a4d6968c857af6e054f88db19b2db
Welington Marquezini Valverde da Silva <welington_marquezini88@live.com>
2022-07-29 19:17:15 -0300

    Ajuste no novo tela

--- src/components/NovoAluno/index.tsx
+++ src/components/NovoAluno/index.tsx
@@ -1,11 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FiCornerDownLeft, FiUserPlus } from "react-icons/fi";
 import { Link, useParams } from "react-router-dom";
+import { createBrowserHistory } from "history";
 import './index.css';
+import { api } from "../../services/api";
 
 export default function NovoAluno(){
+    const [id, setId] = useState(null);
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [idade, setIdade] = useState(0);
     //hooks react
     const {alunoId} = useParams(); 
+    const history = createBrowserHistory();
+    
+    const token = localStorage.getItem("token");
+    const authorization ={
+        headers : {
+            Authorization : `Bearer ${token}`
+        }
+    }
+
+    //hooks user effect
+    async function loadAluno() {
+        try {
+            const response = await api.get(`api/Alunos/${alunoId}`,authorization);
+
+            setId(response.data.id);
+            setNome(response.data.nome);
+            setEmail(response.data.email);
+            setIdade(response.data.idade);
+        } catch (error) {
+            alert("Não foi possível carregar o aluno " + error);
+        }    
+    }
+
+    //hooks user effect
+    useEffect(() => {
+        if (alunoId === '0')
+            return;
+        else
+            loadAluno();
+    }, [alunoId]);
+
     return(
         <div className="novo-aluno-container">
             <div className="content">
@@ -16,9 +53,9 @@ export default function NovoAluno(){
                         <FiCornerDownLeft size={25} color="#FFF" /> Retornar
                     </Link>
                     <form>
-                        <input placeholder="Nome: " />
-                        <input placeholder="Email: " />
-                        <input placeholder="Idade: " />
+                        <input placeholder="Nome: " value={nome} onChange={e => setNome(e.target.value)} />
+                        <input placeholder="Email: " value={email} onChange={e => setEmail(e.target.value)} />
+                        <input placeholder="Idade: " value={idade} onChange={e => setIdade(Number.parseInt(e.target.value))}/>
                         <button className="button" type={"submit"} >
                             {alunoId === '0' ? 'Incluir' : 'Atualizar'  }
                         </button>
